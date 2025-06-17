305bdbda89b92072282dadd3e05bd71d48b7ac24
Hoàng Tấn <thtan0407@gmail.com>
2025-06-17 14:34:55 +0700

    Update

--- website-so-sanh-co-phieu/assets/js/app.js
+++ website-so-sanh-co-phieu/assets/js/app.js
@@ -1,9 +1,9 @@
 ;(function ($) {
 	'use strict';
-
+	
 	const handleFilterSelect = function () {
 		const filterIndustry = $('#filterIndustry');
-
+		
 		if (filterIndustry.length) {
 			filterIndustry.select2({
 				templateResult: function (data, container) {
@@ -19,61 +19,73 @@
 				searchInputPlaceholder: filterIndustry.attr('data-search') ?? '',
 			});
 		}
-
+		
 		const filterStock = $('#filterStock');
-
+		
 		if (filterStock.length) {
-			filterStock.select2({
-				templateResult: function (data, container) {
-					if (data.element) {
-						$(container).addClass($(data.element).attr("class"));
-					}
-					return data.text;
-				},
-				dropdownParent: filterStock.parent(),
-				minimumResultsForSearch: 20,//Infinity
-				language: 'vi',
-				width: '100%',
-				searchInputPlaceholder: filterStock.attr('data-search') ?? '',
+			$.fn.select2.amd.require([ 'select2/i18n/vi' ], function (vi) {
+				// Ghi đè maximumSelected
+				vi.maximumSelected = function (args) {
+					return "Chỉ được chọn 2 mã cổ phiếu";
+				};
+				
+				// Gán lại vào Select2
+				$.fn.select2.defaults.set("language", vi);
+				
+				// Gọi select2
+				filterStock.select2({
+					templateResult: function (data, container) {
+						if (data.element) {
+							$(container).addClass($(data.element).attr("class"));
+						}
+						return data.text;
+					},
+					dropdownParent: filterStock.parent(),
+					minimumResultsForSearch: 20,
+					maximumSelectionLength: 2,
+					language: 'vi',
+					width: '100%',
+					searchInputPlaceholder: filterStock.attr('data-search') ?? '',
+				});
 			});
 		}
 	}
-
+	
 	const handleInitialChart = function () {
 		const chartPreview = $('.chart-preview');
 		if (chartPreview.length) {
 			chartPreview.map((idx, item) => {
 				const chart = $(item)[0].getContext('2d');
-
+				
 				const lineChart = new Chart(chart, {
 					type: 'line',
 					data: {
-						labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+						labels: [ 'Q1', 'Q2', 'Q3', 'Q4' ],
 						datasets: [
 							{
 								label: 'PVS',
-								data: [30, 28, 24, 20],
+								data: [ 30, 28, 24, 20 ],
 								borderColor: '#099444',
 								backgroundColor: '#82ffb8',
 								tension: 0.4
 							},
 							{
 								label: 'YEG',
-								data: [45, 20, 35, 50],
+								data: [ 45, 20, 35, 50 ],
 								borderColor: '#0e6096',
 								backgroundColor: '#86cdff',
 								tension: 0.4
 							},
 							{
 								label: 'CTG',
-								data: [74, 78, 54, 23],
+								data: [ 74, 78, 54, 23 ],
 								borderColor: '#85560b',
 								backgroundColor: '#ffcb7f',
 								tension: 0.4
 							},
 							{
 								label: 'VCB',
-								data: [80, 66, 78, 92],
+								data: [ 80, 66, 78, 92 ],
 								borderColor: '#FF6384',
 								backgroundColor: '#FFB1C1',
 								tension: 0.4
@@ -82,6 +94,7 @@
 					},
 					options: {
 						responsive: true,
+						maintainAspectRatio: false,
 						plugins: {
 							legend: {
 								labels: {
@@ -112,7 +125,7 @@
 									color: '#ffffff',
 									padding: 5
 								},
-								title: {
+								/*title: {
 									display: true,
 									text: 'No of Potentials',
 									color: '#ffffff',
@@ -120,7 +133,7 @@
 										size: 15,
 										weight: 'bold',
 									},
-								}
+								}*/
 							},
 							x: {
 								grid: {
@@ -147,9 +160,69 @@
 			})
 		}
 	}
-
+	
+	const handleInitialTooltip = function () {
+		const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+		const tooltipList = [ ...tooltipTriggerList ].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+	}
+	
+	const handleInitialTab = () => {
+		const tabElement = $('.handleEffectTab');
+		if (tabElement.length > 0) {
+			
+			tabElement.each(function () {
+				const tabItem = $(this);
+				
+				const tabItemBackground = tabItem.find('.handleEffectTabLine');
+				const tabItemButton = tabItem.find('.handleEffectTabItem');
+				const tabItemButtonActive = tabItem.find('.handleEffectTabItem.active')[0];
+				
+				if (tabItemButtonActive != null) {
+					setTimeout(() => {
+						tabItemBackground.css({
+							left: parseInt(tabItemButtonActive.offsetLeft) + "px",
+							width: parseInt(tabItemButtonActive.offsetWidth) + "px",
+							opacity: 1
+						});
+					}, 250)
+					
+					$(window).resize(function () {
+						tabItemBackground.css({
+							left: parseInt(tabItemButtonActive.offsetLeft) + "px",
+							width: parseInt(tabItemButtonActive.offsetWidth) + "px",
+							opacity: 1
+						});
+					});
+					
+					setTimeout(function () {
+						tabItemButton.addClass('is-done');
+						tabItemBackground.addClass('transition-default')
+					}, 300)
+					
+					if (tabItemButton.length) {
+						tabItemButton.each(function () {
+							const tabElement = $(this);
+							tabElement.on("click", function () {
+								tabItemButton.removeClass("active");
+								
+								tabElement.addClass("active");
+								tabItemBackground.css({
+									left: parseInt(tabElement[0].offsetLeft) + "px",
+									width: parseInt(tabElement[0].offsetWidth) + "px",
+									opacity: 1
+								});
+							});
+						})
+					}
+				}
+			})
+		}
+	}
+	
 	$(document).ready(function () {
 		handleFilterSelect();
 		handleInitialChart();
+		handleInitialTooltip();
+		handleInitialTab();
 	});
 })(jQuery);
