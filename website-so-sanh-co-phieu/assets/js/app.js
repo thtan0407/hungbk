38e94aa47caa7301149d2f649cacdf4e967df2a6
Hoàng Tấn <thtan0407@gmail.com>
2025-06-21 10:25:08 +0700

    Update

--- website-so-sanh-co-phieu/assets/js/app.js
+++ website-so-sanh-co-phieu/assets/js/app.js
@@ -1,91 +1,192 @@
 ;(function ($) {
 	'use strict';
-
-	const handleFilterSelect = function () {
-		const filterIndustry = $('#filterIndustry');
-
-		if (filterIndustry.length) {
-			filterIndustry.select2({
-				templateResult: function (data, container) {
-					if (data.element) {
-						$(container).addClass($(data.element).attr("class"));
-					}
-					return data.text;
-				},
-				dropdownParent: filterIndustry.parent(),
-				minimumResultsForSearch: 20,//Infinity
-				language: 'vi',
-				width: '100%',
-				searchInputPlaceholder: filterIndustry.attr('data-search') ?? '',
-			});
+	let dataIndustry = [];
+	const filterIndustry = $('#filterIndustry');
+	const filterStock1 = $('#filterStock1');
+	const filterStock2 = $('#filterStock2');
+	
+	const Toast = Swal.mixin({
+		toast: true,
+		position: "top-end",
+		showConfirmButton: false,
+		timer: 3000,
+		timerProgressBar: true,
+		didOpen: (toast) => {
+			toast.onmouseenter = Swal.stopTimer;
+			toast.onmouseleave = Swal.resumeTimer;
 		}
-
-		const filterStock = $('#filterStock');
-
-		if (filterStock.length) {
-			$.fn.select2.amd.require(['select2/i18n/vi'], function (vi) {
-				// Ghi đè maximumSelected
-				vi.maximumSelected = function (args) {
-					return "Chỉ được chọn 2 mã cổ phiếu";
-				};
-
-				// Gán lại vào Select2
-				$.fn.select2.defaults.set("language", vi);
-
-				// Gọi select2
-				filterStock.select2({
-					templateResult: function (data, container) {
-						if (data.element) {
-							$(container).addClass($(data.element).attr("class"));
-						}
-						return data.text;
-					},
-					dropdownParent: filterStock.parent(),
-					minimumResultsForSearch: 20,
-					maximumSelectionLength: 2,
-					language: 'vi',
-					width: '100%',
-					searchInputPlaceholder: filterStock.attr('data-search') ?? '',
+	});
+	const handleLoadPage = function () {
+		const pathJson = './assets/json/stock.json';
+		
+		$.getJSON(pathJson)
+			.done(function (data) {
+				if (Object.keys(data).length > 0) {
+					dataIndustry = data;
+					handleFilterSelect();
+					handleChangeIndustry();
+					handleSubmitCompare();
+				} else {
+					Toast.fire({
+						icon: "error",
+						title: "Có lỗi khi tải trang, vui lòng thử lại"
+					});
+					
+					setTimeout(() => window.location.reload(), 3000);
+				}
+			})
+			.fail(function () {
+				Toast.fire({
+					icon: "error",
+					title: "Có lỗi khi tải trang, vui lòng thử lại"
 				});
+				
+				setTimeout(() => window.location.reload(), 3000);
 			});
-		}
 	}
-
+	
+	const handleFilterSelect = function () {
+		filterIndustry.select2({
+			templateResult: function (data, container) {
+				if (data.element) {
+					$(container).addClass($(data.element).attr("class"));
+				}
+				return data.text;
+			},
+			dropdownParent: filterIndustry.parent(),
+			minimumResultsForSearch: 20,//Infinity
+			language: 'vi',
+			width: '100%',
+			searchInputPlaceholder: filterIndustry.attr('data-search') ?? '',
+		});
+		
+		filterStock1.select2({
+			templateResult: function (data, container) {
+				if (data.element) {
+					$(container).addClass($(data.element).attr("class"));
+				}
+				return data.text;
+			},
+			dropdownParent: filterStock1.parent(),
+			minimumResultsForSearch: 20,//Infinity
+			language: 'vi',
+			width: '100%',
+			searchInputPlaceholder: filterStock1.attr('data-search') ?? '',
+		});
+		
+		filterStock2.select2({
+			templateResult: function (data, container) {
+				if (data.element) {
+					$(container).addClass($(data.element).attr("class"));
+				}
+				return data.text;
+			},
+			dropdownParent: filterStock2.parent(),
+			minimumResultsForSearch: 20,//Infinity
+			language: 'vi',
+			width: '100%',
+			searchInputPlaceholder: filterStock2.attr('data-search') ?? '',
+		});
+	}
+	
+	const handleChangeIndustry = function () {
+		filterIndustry.change(() => {
+			if (filterIndustry.val() > 0 && dataIndustry[filterIndustry.val()] !== undefined) {
+				let optionStock = '<option value="-1">Chọn mã cổ phiếu</option>';
+				dataIndustry[filterIndustry.val()].map(function (item, idx) {
+					optionStock += `<option value="${ idx }">${ item }</option>`;
+				});
+				
+				filterStock1.html(optionStock).trigger('change.select2');
+				filterStock2.html(optionStock).trigger('change.select2');
+			} else {
+				Toast.fire({
+					icon: "error",
+					title: "Có lỗi khi tải trang, vui lòng thử lại"
+				});
+				
+				setTimeout(() => window.location.reload(), 3000);
+			}
+		})
+	}
+	
+	const handleSubmitCompare = function () {
+		$('#submitCompare').click(function () {
+			const filterIndustryValue = $('#filterIndustry').val();
+			const filterStock1Value = $('#filterStock1').val();
+			const filterStock2Value = $('#filterStock2').val();
+			
+			if (dataIndustry[filterIndustryValue] === undefined) {
+				Toast.fire({
+					icon: "error",
+					title: "Vui lòng chọn ngành nghề."
+				});
+				
+				return false;
+			}
+			
+			if (parseInt(filterStock1Value) === -1 || parseInt(filterStock2Value) === -1 || dataIndustry[filterIndustryValue][filterStock1Value] === undefined) {
+				Toast.fire({
+					icon: "error",
+					title: "Vui lòng chọn mã cổ phiếu. Lưu ý 2 mã cổ phiếu phải khác nhau"
+				});
+				
+				return false;
+			}
+			
+			if (parseInt(filterStock1Value) === parseInt(filterStock2Value)) {
+				Toast.fire({
+					icon: "error",
+					title: "Vui lòng chọn 2 mã cổ phiếu khác nhau."
+				});
+				
+				return false;
+			}
+			
+			
+			$('#filterResult').show();
+			handleInitialChart();
+			handleInitialTooltip();
+			handleInitialTab();
+			handleCollapseTable();
+		});
+	}
+	
 	const handleInitialChart = function () {
 		const chartPreview = $('.chart-preview');
 		if (chartPreview.length) {
 			chartPreview.map((idx, item) => {
 				const chart = $(item)[0].getContext('2d');
-
+				
 				const lineChart = new Chart(chart, {
 					type: 'line',
 					data: {
-						labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+						labels: [ 'Q1', 'Q2', 'Q3', 'Q4' ],
 						datasets: [
 							{
 								label: 'PVS',
-								data: [30, 28, 24, 20],
+								data: [ 30, 28, 24, 20 ],
 								borderColor: '#099444',
 								backgroundColor: '#82ffb8',
 								tension: 0.4
 							},
 							{
 								label: 'YEG',
-								data: [45, 20, 35, 50],
+								data: [ 45, 20, 35, 50 ],
 								borderColor: '#0e6096',
 								backgroundColor: '#86cdff',
 								tension: 0.4
 							},
 							{
 								label: 'CTG',
-								data: [74, 78, 54, 23],
+								data: [ 74, 78, 54, 23 ],
 								borderColor: '#85560b',
 								backgroundColor: '#ffcb7f',
 								tension: 0.4
 							},
 							{
 								label: 'VCB',
-								data: [80, 66, 78, 92],
+								data: [ 80, 66, 78, 92 ],
 								borderColor: '#FF6384',
 								backgroundColor: '#FFB1C1',
 								tension: 0.4
@@ -161,23 +262,23 @@
 			})
 		}
 	}
-
+	
 	const handleInitialTooltip = function () {
 		const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-		const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+		const tooltipList = [ ...tooltipTriggerList ].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 	}
-
+	
 	const handleInitialTab = () => {
 		const tabElement = $('.handleEffectTab');
 		if (tabElement.length > 0) {
-
+			
 			tabElement.each(function () {
 				const tabItem = $(this);
-
+				
 				const tabItemBackground = tabItem.find('.handleEffectTabLine');
 				const tabItemButton = tabItem.find('.handleEffectTabItem');
 				const tabItemButtonActive = tabItem.find('.handleEffectTabItem.active')[0];
-
+				
 				if (tabItemButtonActive != null) {
 					setTimeout(() => {
 						tabItemBackground.css({
@@ -186,7 +287,7 @@
 							opacity: 1
 						});
 					}, 250)
-
+					
 					$(window).resize(function () {
 						tabItemBackground.css({
 							left: parseInt(tabItemButtonActive.offsetLeft) + "px",
@@ -194,18 +295,18 @@
 							opacity: 1
 						});
 					});
-
+					
 					setTimeout(function () {
 						tabItemButton.addClass('is-done');
 						tabItemBackground.addClass('transition-default')
 					}, 300)
-
+					
 					if (tabItemButton.length) {
 						tabItemButton.each(function () {
 							const tabElement = $(this);
 							tabElement.on("click", function () {
 								tabItemButton.removeClass("active");
-
+								
 								tabElement.addClass("active");
 								tabItemBackground.css({
 									left: parseInt(tabElement[0].offsetLeft) + "px",
@@ -219,7 +320,7 @@
 			})
 		}
 	}
-
+	
 	const handleCollapseTable = function () {
 		if ($('.chartCollapseButton').length) {
 			$('.chartCollapseButton').click(function () {
@@ -235,12 +336,9 @@
 			});
 		}
 	}
-
+	
 	$(document).ready(function () {
-		handleFilterSelect();
-		handleInitialChart();
-		handleInitialTooltip();
-		handleInitialTab();
-		handleCollapseTable();
+		handleLoadPage();
 	});
-})(jQuery);
+})
+(jQuery);
