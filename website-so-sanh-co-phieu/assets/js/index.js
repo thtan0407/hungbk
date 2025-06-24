c395f62408e6d4595e1b15d53c812596462ffda7
thtan0407 <thtan0407@gmail.com>
2025-06-24 20:53:50 +0700

    Update

--- website-so-sanh-co-phieu/assets/js/index.js
+++ website-so-sanh-co-phieu/assets/js/index.js
@@ -5,6 +5,8 @@
 	let chartFields = {};
 	let chartPreview1 = null;
 	let chartPreview2 = null;
+	let dataRaw1 = null;
+	let dataRaw2 = null;
 	let filterIndustryValue = null
 	let filterStock1Value = null
 	let filterStock2Value = null
@@ -12,23 +14,22 @@
 	const filterStock1 = $('#filterStock1');
 	const filterStock2 = $('#filterStock2');
 	const filterResult = $('#filterResult');
-	
+
 	const handleLoadPage = function () {
 		const pathJson = './assets/json/stock.json';
-		
+
 		$.getJSON(pathJson)
 			.done(function (data) {
 				if (Object.keys(data).length > 0) {
 					dataIndustry = data;
 					handleFilterSelect();
 					handleChangeIndustry();
-					handleSubmitCompare();
 				} else {
 					Toast.fire({
 						icon: "error",
 						title: "Có lỗi khi tải trang, vui lòng thử lại"
 					});
-					
+
 					setTimeout(() => window.location.reload(), 3000);
 				}
 			})
@@ -37,11 +38,11 @@
 					icon: "error",
 					title: "Có lỗi khi tải trang, vui lòng thử lại"
 				});
-				
+
 				setTimeout(() => window.location.reload(), 3000);
 			});
 	}
-	
+
 	const handleFilterSelect = function () {
 		filterIndustry.select2({
 			templateResult: function (data, container) {
@@ -56,7 +57,7 @@
 			width: '100%',
 			searchInputPlaceholder: filterIndustry.attr('data-search') ?? '',
 		});
-		
+
 		filterStock1.select2({
 			templateResult: function (data, container) {
 				if (data.element) {
@@ -70,7 +71,7 @@
 			width: '100%',
 			searchInputPlaceholder: filterStock1.attr('data-search') ?? '',
 		});
-		
+
 		filterStock2.select2({
 			templateResult: function (data, container) {
 				if (data.element) {
@@ -85,15 +86,15 @@
 			searchInputPlaceholder: filterStock2.attr('data-search') ?? '',
 		});
 	}
-	
+
 	const handleChangeIndustry = function () {
 		filterIndustry.change(() => {
 			if (filterIndustry.val() > 0 && dataIndustry[filterIndustry.val()] !== undefined) {
 				let optionStock = '<option value="-1">Chọn mã cổ phiếu</option>';
 				dataIndustry[filterIndustry.val()].map(function (item, idx) {
-					optionStock += `<option value="${ item }">${ item }</option>`;
+					optionStock += `<option value="${item}">${item}</option>`;
 				});
-				
+
 				filterStock1.html(optionStock).trigger('change.select2');
 				filterStock2.html(optionStock).trigger('change.select2');
 			} else {
@@ -101,129 +102,90 @@
 					icon: "error",
 					title: "Có lỗi khi tải trang, vui lòng thử lại"
 				});
-				
+
 				setTimeout(() => window.location.reload(), 3000);
 			}
 		})
 	}
-	
-	const handleSubmitCompare = function () {
-		$('#submitCompare').click(function () {
-			const buttonCompare = $(this);
-			
-			if (filterIndustryValue == $('#filterIndustry').val() && filterStock1Value == $('#filterStock1').val() && filterStock2Value == $('#filterStock2').val()) {
-				return null;
-			}
-			
-			buttonCompare.prop('disabled', true);
-			
-			filterIndustryValue = $('#filterIndustry').val();
-			filterStock1Value = $('#filterStock1').val();
-			filterStock2Value = $('#filterStock2').val();
-			
-			if (dataIndustry[filterIndustryValue] === undefined) {
+
+	const handleSubmitCompare = function (period = 'quy') {
+		const urlAPI = `http://20.89.170.203:8000/mapping-bctc`;
+
+		fetch(urlAPI)
+			.then(response => response.json())
+			.then(data => {
+				if (Object.keys(data).length > 0) {
+					mappingBCTC = data[filterIndustryValue];
+
+					chartFields = mappingBCTC.colunm_chart.map((key, index) => ({
+						key: key,
+						label: mappingBCTC.column_mapping[key] || key,
+						borderColor: arrColors[index % arrColors.length].borderColor,
+						background: arrColors[index % arrColors.length].background
+					}));
+
+					// Gọi song song 2 cổ phiếu
+					return Promise.all([
+						handleRenderStock(filterIndustryValue, filterStock1Value, period, '#chartPreview', chartPreview1),
+						handleRenderStock(filterIndustryValue, filterStock2Value, period, '#chartPreview2', chartPreview2)
+					]);
+				} else {
+					throw new Error("Dữ liệu cấu hình trống");
+				}
+			})
+			.then(([dataChart1, dataChart2]) => {
+				if (!dataChart1 || !dataChart2) {
+					throw new Error("Thiếu dữ liệu biểu đồ");
+				}
+
+				// Lưu chart để sử dụng sau này
+				chartPreview1 = dataChart1.chart;
+				chartPreview2 = dataChart2.chart;
+
+				dataRaw1 = dataChart1.rawData;
+				dataRaw2 = dataChart2.rawData;
+
+				// Render bảng
+				handleRenderTable(dataRaw1, filterIndustryValue, filterStock1Value, period, '#tablePreview');
+				handleRenderTable(dataRaw2, filterIndustryValue, filterStock2Value, period, '#tablePreview2');
+
+				// Cập nhật UI
+				filterResult.show();
+				filterResult.find('.nameStock1').html(filterStock1Value);
+				filterResult.find('.nameStock2').html(filterStock2Value);
+				handleChangePeriod();
+				handleInitialTab();
+				$('#submitCompare').prop('disabled', false);
+			})
+			.catch(error => {
 				Toast.fire({
 					icon: "error",
-					title: "Vui lòng chọn ngành nghề."
+					title: "Đã xảy ra lỗi khi xử lý dữ liệu. Vui lòng thử lại!"
 				});
-				buttonCompare.prop('disabled', false);
-				return false;
-			}
-			
-			if (parseInt(filterStock1Value) === -1 || parseInt(filterStock2Value) === -1) {
-				Toast.fire({
-					icon: "error",
-					title: "Vui lòng chọn mã cổ phiếu. Lưu ý 2 mã cổ phiếu phải khác nhau"
-				});
-				buttonCompare.prop('disabled', false);
-				return false;
-			}
-			
-			if (filterStock1Value === filterStock2Value) {
-				Toast.fire({
-					icon: "error",
-					title: "Vui lòng chọn 2 mã cổ phiếu khác nhau."
-				});
-				buttonCompare.prop('disabled', false);
-				return false;
-			}
-			
-			const urlAPI = `http://20.89.170.203:8000/mapping-bctc`;
-			
-			fetch(urlAPI)
-				.then(response => response.json())
-				.then(data => {
-					if (Object.keys(data).length > 0) {
-						mappingBCTC = data[filterIndustryValue];
-						
-						chartFields = mappingBCTC.colunm_chart.map((key, index) => ({
-							key: key,
-							label: mappingBCTC.column_mapping[key] || key,
-							borderColor: arrColors[index % arrColors.length].borderColor,
-							background: arrColors[index % arrColors.length].background
-						}));
-						
-						// Gọi song song 2 cổ phiếu
-						return Promise.all([
-							handleRenderStock(filterIndustryValue, filterStock1Value, 'quy', '#chartPreview', chartPreview1),
-							handleRenderStock(filterIndustryValue, filterStock2Value, 'quy', '#chartPreview2', chartPreview2)
-						]);
-					} else {
-						throw new Error("Dữ liệu cấu hình trống");
-					}
-				})
-				.then(([ rawData1, rawData2 ]) => {
-					if (!rawData1 || !rawData2) {
-						throw new Error("Thiếu dữ liệu biểu đồ");
-					}
-					
-					// Lưu chart để sử dụng sau này
-					chartPreview1 = rawData1.chart;
-					chartPreview2 = rawData2.chart;
-					
-					// Render bảng
-					handleRenderTable(rawData1.rawData, filterIndustryValue, filterStock1Value, 'quy', '#tablePreview');
-					handleRenderTable(rawData2.rawData, filterIndustryValue, filterStock2Value, 'quy', '#tablePreview2');
-					
-					// Cập nhật UI
-					filterResult.show();
-					filterResult.find('.nameStock1').html(filterStock1Value);
-					filterResult.find('.nameStock2').html(filterStock2Value);
-					
-					handleChangePeriod();
-					handleInitialTab();
-					buttonCompare.prop('disabled', false);
-				})
-				.catch(error => {
-					Toast.fire({
-						icon: "error",
-						title: "Đã xảy ra lỗi khi xử lý dữ liệu. Vui lòng thử lại!"
-					});
-					console.error("Lỗi tổng:", error);
-					buttonCompare.prop('disabled', false);
-					setTimeout(() => window.location.reload(), 3000);
-				});
-		});
+				$('#submitCompare').prop('disabled', false);
+				console.error("Lỗi tổng:", error);
+				setTimeout(() => window.location.reload(), 3000);
+			});
 	}
-	
+
 	const handleRenderStock = (stockType, stockCode, chartType = 'quy', chartItem = '', chartPreview) => {
-		const urlAPI = `http://20.89.170.203:8000/stocks?stockCode=${ stockCode }&stockType=${ stockType }`;
-		
+		const urlAPI = `http://20.89.170.203:8000/stocks?stockCode=${stockCode}&stockType=${stockType}`;
+
 		return fetch(urlAPI)
 			.then(response => {
-				if (!response.ok) throw new Error(`HTTP status ${ response.status }`);
+				if (!response.ok) throw new Error(`HTTP status ${response.status}`);
 				return response.json();
 			})
 			.then(data => {
 				if (data[chartType] && data[chartType].length > 0) {
 					const rawData = data[chartType];
-					const chart = handleInitialChart(stockCode, rawData, chartItem, chartPreview);
-					
+					const chart = handleInitialChart(stockCode, rawData, chartItem, chartType, chartPreview);
+
 					return {
 						chart,
 						rawData
 					};
-					
+
 				} else {
 					Toast.fire({
 						icon: "error",
@@ -241,7 +203,7 @@
 				return null;
 			});
 	};
-	
+
 	const arrColors = [
 		{
 			borderColor: '#099444',
@@ -264,7 +226,7 @@
 			background: '#FFA0B4'
 		}
 	]
-	
+
 	const generateDataSets = (dataStock) => {
 		return chartFields.map(field => ({
 			label: field.label,
@@ -274,32 +236,36 @@
 			tension: 0.4
 		}));
 	}
-	
-	const generateLabels = (dataStock) => {
-		return dataStock.map(item => `Q${ item.ky } - ${ item.nam }`);
+
+	const generateLabels = (dataStock, chartType) => {
+		if (chartType === 'nam') {
+			return dataStock.map(item => `Năm ${item.nam}`);
+		} else {
+			return dataStock.map(item => `Q${item.ky} - ${item.nam}`);
+		}
 	}
-	
-	const handleInitialChart = function (stockCode, stockValue, chartItem, chartPreview) {
+
+	const handleInitialChart = function (stockCode, stockValue, chartItem, chartType, chartPreview) {
 		const chart = $(chartItem);
 		if (!chart.length) return null;
-		
-		
+
+
 		const chx = chart[0].getContext('2d');
-		
-		const labels = generateLabels(stockValue);
+
+		const labels = generateLabels(stockValue, chartType);
 		const dataSets = generateDataSets(stockValue);
-		
+
 		const divisor = 1_000_000_000;
-		
+
 		const allValues = dataSets.flatMap(ds => ds.data);
 		const minY = Math.min(...allValues) / divisor;
 		const maxY = Math.max(...allValues) / divisor;
-		
+
 		const convertedDatasets = dataSets.map(ds => ({
 			...ds,
 			data: ds.data.map(val => val / divisor)
 		}));
-		
+
 		// Update lại chart nếu gọi từ lần 2
 		if (chartPreview && chartPreview instanceof Chart) {
 			chartPreview.data.labels = labels;
@@ -310,7 +276,7 @@
 			chartPreview.update();
 			return chartPreview;
 		}
-		
+
 		const chartInit = new Chart(chx, {
 			type: 'line',
 			data: {
@@ -337,10 +303,10 @@
 							label: function (context) {
 								const label = context.dataset.label || '';
 								const value = context.parsed.y;
-								return `${ label }: ${ value.toLocaleString('en-US', {
+								return `${label}: ${value.toLocaleString('en-US', {
 									minimumFractionDigits: 2,
 									maximumFractionDigits: 2
-								}) } tỷ`;
+								})} tỷ`;
 							}
 						}
 					},
@@ -390,10 +356,10 @@
 				}
 			}
 		});
-		
+
 		return chartInit
 	}
-	
+
 	const getLatestTime = (data, count = 4) => {
 		return data
 			.filter(item => item.nam && item.ky)
@@ -403,58 +369,58 @@
 			})
 			.slice(0, count);
 	};
-	
+
 	const formatNumber = function (val) {
 		if (val == null) return '-';
 		const num = Number(val);
 		if (isNaN(num)) return val;
-		let formattedNumber = (num / 1_000_000).toLocaleString('en-US', { maximumFractionDigits: 0 });
-		
+		let formattedNumber = (num / 1_000_000).toLocaleString('en-US', {maximumFractionDigits: 0});
+
 		if (formattedNumber === '-0') {
 			formattedNumber = '0';
 		}
-		
+
 		return formattedNumber;
 	};
-	
+
 	const handleRenderTable = function (data, stockType, stockCode, chartType, tablePreview) {
 		$(tablePreview + ' .tablePreviewPeriod').html(chartType === 'quy' ? "quý" : "năm");
-		
+
 		const sortedData = getLatestTime(data, 4);
-		
+
 		$(tablePreview + ' .tablePreviewTime').empty();
-		
+
 		sortedData.forEach(item => {
 			if (item.nam && item.ky) {
 				const $item = $(`
 				<div class="chart-table_time__list___item">
-					<span>Quý ${ item.ky }/${ item.nam }</span>
+					<span>Quý ${item.ky}/${item.nam}</span>
 					<span>Hợp nhất/CKT</span>
 				</div>
 			`);
 				$(tablePreview + ' .tablePreviewTime').append($item);
 			}
 		});
-		
+
 		$(tablePreview + ' .tablePreviewBody').empty();
 		let renderRow = `<div class="chart-table_item">
 												<div class="chart-table_item__heading">
 													Kết quả kinh doanh
 												</div>`;
-		
+
 		const columnMapping = mappingBCTC.column_mapping;
-		
-		const keysToShow = Object.keys(columnMapping).filter(key => ![ 'cp', 'nam', 'ky' ].includes(key));
-		
+
+		const keysToShow = Object.keys(columnMapping).filter(key => !['cp', 'nam', 'ky'].includes(key));
+
 		keysToShow.forEach(fieldKey => {
 			renderRow += `
 			<div class="chart-table_item__child">
 				<div class="chart-table_item__wrapper">
-					<div class="chart-table_item__title">${ columnMapping[fieldKey] }</div>
+					<div class="chart-table_item__title">${columnMapping[fieldKey]}</div>
 					<div class="chart-table_item__row">`;
 			sortedData.forEach(item => {
 				const val = formatNumber(item[fieldKey]);
-				renderRow += `<div class="chart-table_item__column">${ val.toLocaleString('en-US') }</div>`;
+				renderRow += `<div class="chart-table_item__column">${val.toLocaleString('en-US')}</div>`;
 			});
 			renderRow += `
 					</div>
@@ -462,33 +428,44 @@
 			</div>
 		`;
 		});
-		
+
 		renderRow += `</div>`;
 		$(tablePreview + ' .tablePreviewBody').html(renderRow)
 	}
-	
+
 	const handleChangePeriod = function () {
 		const buttonChangePeriod = $('.handleChangePeriod');
 		if (!buttonChangePeriod.length) {
 			return false;
 		}
-		
-		buttonChangePeriod.click(function () {
-		
+
+		buttonChangePeriod.off().click(function () {
+			if ($(this).hasClass('active') || ![1, 2].includes(parseInt($(this).attr('data-value')))) {
+				return false;
+			}
+
+			let periodValue = parseInt($(this).attr('data-value')) === 2 ? 'nam' : 'quy';
+
+			buttonChangePeriod.removeClass('active button-theme').addClass('button-theme_secondary');
+			$(this).addClass('active button-theme').removeClass('button-theme_secondary');
+
+			handleSubmitCompare(periodValue);
+
+			$('#submitCompare').attr('data-value', parseInt($(this).attr('data-value')))
 		});
 	}
-	
+
 	const handleInitialTab = () => {
 		const tabElement = $('.handleEffectTab');
 		if (tabElement.length > 0) {
-			
+
 			tabElement.each(function () {
 				const tabItem = $(this);
-				
+
 				const tabItemBackground = tabItem.find('.handleEffectTabLine');
 				const tabItemButton = tabItem.find('.handleEffectTabItem');
 				const tabItemButtonActive = tabItem.find('.handleEffectTabItem.active')[0];
-				
+
 				if (tabItemButtonActive != null) {
 					setTimeout(() => {
 						tabItemBackground.css({
@@ -497,7 +474,7 @@
 							opacity: 1
 						});
 					}, 250)
-					
+
 					$(window).resize(function () {
 						tabItemBackground.css({
 							left: parseInt(tabItemButtonActive.offsetLeft) + "px",
@@ -505,34 +482,95 @@
 							opacity: 1
 						});
 					});
-					
+
 					setTimeout(function () {
 						tabItemButton.addClass('is-done');
 						tabItemBackground.addClass('transition-default')
 					}, 300)
-					
+
 					if (tabItemButton.length) {
 						tabItemButton.each(function () {
-							const tabElement = $(this);
-							tabElement.on("click", function () {
-								tabItemButton.removeClass("active");
-								
-								tabElement.addClass("active");
-								tabItemBackground.css({
-									left: parseInt(tabElement[0].offsetLeft) + "px",
-									width: parseInt(tabElement[0].offsetWidth) + "px",
-									opacity: 1
+							const clickedTab = $(this);
+
+							clickedTab.on("click", function () {
+								if (clickedTab.hasClass('active')) return false;
+
+								const value = clickedTab.attr('data-value');
+
+								// ✅ Lặp qua tất cả các block tab
+								$('.handleEffectTab').each(function () {
+									const tabBlock = $(this);
+									const tabButtons = tabBlock.find('.handleEffectTabItem');
+									const tabBackground = tabBlock.find('.handleEffectTabLine');
+									const targetTab = tabButtons.filter(`[data-value="${value}"]`);
+
+									if (!targetTab.length) return;
+
+									tabButtons.removeClass('active');
+									targetTab.addClass('active');
+
+									tabBackground.css({
+										left: targetTab[0].offsetLeft + "px",
+										width: targetTab[0].offsetWidth + "px",
+										opacity: 1
+									});
 								});
+
+								// Gọi sự kiện change filter nếu cần
+								$(`.handleChangePeriod[data-value="${value}"]`).trigger('click');
 							});
-						})
+						});
 					}
 				}
 			})
 		}
 	}
-	
+
 	$(document).ready(function () {
 		handleLoadPage();
+
+		$('#submitCompare').click(function () {
+			const buttonCompare = $(this);
+
+			if (filterIndustryValue == $('#filterIndustry').val() && filterStock1Value == $('#filterStock1').val() && filterStock2Value == $('#filterStock2').val()) {
+				return null;
+			}
+
+			buttonCompare.prop('disabled', true);
+
+			filterIndustryValue = $('#filterIndustry').val();
+			filterStock1Value = $('#filterStock1').val();
+			filterStock2Value = $('#filterStock2').val();
+
+			if (dataIndustry[filterIndustryValue] === undefined) {
+				Toast.fire({
+					icon: "error",
+					title: "Vui lòng chọn ngành nghề."
+				});
+				buttonCompare.prop('disabled', false);
+				return false;
+			}
+
+			if (parseInt(filterStock1Value) === -1 || parseInt(filterStock2Value) === -1) {
+				Toast.fire({
+					icon: "error",
+					title: "Vui lòng chọn mã cổ phiếu. Lưu ý 2 mã cổ phiếu phải khác nhau"
+				});
+				buttonCompare.prop('disabled', false);
+				return false;
+			}
+
+			if (filterStock1Value === filterStock2Value) {
+				Toast.fire({
+					icon: "error",
+					title: "Vui lòng chọn 2 mã cổ phiếu khác nhau."
+				});
+				buttonCompare.prop('disabled', false);
+				return false;
+			}
+			let periodValue = parseInt(buttonCompare.attr('data-value')) === 2 ? 'nam' : 'quy';
+			handleSubmitCompare(periodValue)
+		});
 	});
 })
 (jQuery);
